cf16171b2ed017fbbe930dbee8fec27b04b412de
Aria <82889549+AriaMehrafza@users.noreply.github.com>
2026-05-10 16:31:05 +0330

    feat: Add channel section
    Users can specify desired channels in config file instead of main for the bot to check continuously.

--- config.py
+++ config.py
@@ -4,3 +4,11 @@ BALE_TOKEN = # Paste your Bale bot token here (in Bale, you can make bots using 
 # Bale UIDs
 CHNL_UID = # Paste the UID of the channel/group where the bot should send & receive the datas in
 DATAS_UID = # Paste the UID of the channel/group where the bot should forward the messages in
+
+# Channels
+# Format: <CHANNEL_USERNAME>: <LIMIT> 
+# NOTE: Limit is the number of messages you want the bot to check each time from that channel
+CHANNELS = {
+    # Example:
+    "bbcpersian": 1,
+}
