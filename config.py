af652f1f50ffc6aa630faba7ebf7b1ae5746848b
Aria <82889549+AriaMehrafza@users.noreply.github.com>
2026-05-10 16:05:24 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Tokens
+BALE_TOKEN = # Paste your Bale bot token here (in Bale, you can make bots using @BotFather)
+
+# Bale UIDs
+CHNL_UID = # Paste the UID of the channel/group where the bot should send & receive the datas in
+DATAS_UID = # Paste the UID of the channel/group where the bot should forward the messages in
